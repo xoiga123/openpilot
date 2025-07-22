d648733ef6ec810aa1c93833b4b6d4978c86a397
Ngô Việt Hoài Bảo <baonvh@phenikaa-x.com>
2025-07-22 15:47:09 +0700

    test

--- common/util.cc
+++ common/util.cc
@@ -79,6 +79,7 @@ std::string read_file(const std::string& fn) {
   if (f.is_open()) {
     f.seekg(0, std::ios::end);
     int size = f.tellg();
+    size = size;
     if (f.good() && size > 0) {
       std::string result(size, '\0');
       f.seekg(0, std::ios::beg);
